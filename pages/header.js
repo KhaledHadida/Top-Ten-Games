cc55cf10f45e31284f6b40a8dca19b74d6aabb20
Majed Hadida <majedhadida@hotmail.com>
2025-01-13 11:45:03 -0500

    missed

--- pages/header.js
+++ pages/header.js
@@ -76,10 +76,10 @@ export default function Header() {
 (<>
     <nav class="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
         <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
-            <a href='/' class="flex items-center">
+            <Link href='/' class="flex items-center">
                 <img src="/Images/TTG.png" class="mr-3 h-6 sm:h-9" alt="TTG Logo" />
                 <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Top Ten Games</span>
-            </a>
+            </Link>
             <div class="flex items-center lg:order-2">
                 <Link href='/signin' class="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800 hover:cursor-pointer hover:text-[#0036B1] flex flex-row items-center gap-3">Sign in <FaSignInAlt size={20}/></Link>
             </div>
